4d07160b8f31982d7abe0b964090d5016f65611e
Per-Frode Pedersen <per.pedersen@bouvet.no>
2016-03-24 17:26:47 +0100

    Added resource planning web app

---
+++ src/GAB.Web.ResourcePlanning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GAB.Web.ResourcePlanning.Startup))]
+namespace GAB.Web.ResourcePlanning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
