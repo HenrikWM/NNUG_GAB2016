58f3837858c67a666bfa52fcc06cb40f507707b3
Per-Frode Pedersen <per.pedersen@bouvet.no>
2016-04-12 23:52:01 +0200

    Added registration for ResourcePlans

---
+++ src/GAB.Http/ApiClients/PlanningApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GAB.Core.Domain;
+
+namespace GAB.Http.ApiClients
+{
+    public class PlanningApiClient : BaseApiClient<ResourcePlan>
+    {
+        public PlanningApiClient(string baseUrl)  : base(baseUrl) 
+        {
+            
+        }
+
+
+        public async Task<IEnumerable<ResourcePlan>> Get()
+        {
+            string url = "/api/resourceplans";
+
+            return await Get(url);
+        }
+
+        public async Task<ResourcePlan> GetById(string id)
+        {
+            string url = string.Format("/api/resourceplans/{0}", id);
+
+            return await GetById(url, id);
+        }
+
+        public async Task<ResourcePlan> Create(ResourcePlan resourcePlan)
+        {
+            string url = "/api/resourceplans";
+
+            return await Create(url, resourcePlan);
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            string url = string.Format("/api/resourceplans/{0}", id);
+
+            return await Delete(url, id);
+        }
+
+        public async Task<bool> Update(ResourcePlan resourcePlan)
+        {
+            string url = "/api/resourceplans";
+
+            return await Update(url, resourcePlan);
+        }
+    
+}
+}
---
+++ src/GAB.Web.ResourcePlanning/Controllers/PlanningController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GAB.Core.Domain;
+using GAB.Http.ApiClients;
+using GAB.Web.ResourcePlanning.Models;
+
+namespace GAB.Web.ResourcePlanning.Controllers
+{
+    public class PlanningController : Controller
+    {
+        // GET: Planning
+        public async Task<ActionResult> Index()
+        {
+
+            EmployeeRecordsApiClient client = new EmployeeRecordsApiClient(ConfigurationManager.AppSettings["EmployeeRecordsApiBaseUrl"]);
+            var employees = await client.Get();
+            var viewmodel = new WorkPlanViewModel();
+            viewmodel.Employees = employees;
+            return View(viewmodel);
+        }
+        
+
+        public async Task<ActionResult> Save(WorkPlanViewModel viewmodel)
+        {
+            PlanningApiClient client = new PlanningApiClient(ConfigurationManager.AppSettings["PlanningApiBaseUrl"]);
+            if (viewmodel.Id == null)
+            {
+                await client.Create(new ResourcePlan
+                {
+                    Id = viewmodel.Id,
+                    EmployeeId = viewmodel.EmployeeId,
+                    From = viewmodel.StartTime,
+                    To = viewmodel.EndTime
+
+                });
+            }
+            else
+            {
+                await client.Create(new ResourcePlan
+                {
+                    Id = viewmodel.Id,
+                    EmployeeId = viewmodel.EmployeeId,
+                    From = viewmodel.StartTime,
+                    To = viewmodel.EndTime
+
+                });
+            }
+
+            return RedirectToAction("Index", "Planning");
+
+        }
+    }
+}
